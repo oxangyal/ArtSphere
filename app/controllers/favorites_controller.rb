d53b717803e81474e6861655c8d503b7e0148e55
Oxana Michkasova <oxana.mitch@gmail.com>
2024-10-07 19:15:16 -0400

    Redirect pages after favorites clicked

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,25 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
     @product = Product.find(params[:product_id])
     current_user.favorites.create(product: @product)
-    redirect_to @product, notice: 'Added to favorites.'
+    redirect_back fallback_location: products_path, notice: 'Added to favorites.'
   end
 
   def destroy
     @product = Product.find(params[:product_id])
     favorite = current_user.favorites.find_by(product: @product)
     favorite.destroy if favorite
-    redirect_to @product, notice: 'Removed from favorites.'
+    redirect_back fallback_location: products_path, notice: 'Removed from favorites.'
+  end
+
+  private
+
+  def authenticate_user!
+    unless user_signed_in?
+      flash[:alert] = 'You need to be registered or logged in to add favorites.'
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
